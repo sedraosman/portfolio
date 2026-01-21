0723eaa66fe41d3410d44c5127b30b4721090b62
sedra <siro5122001@gmail.com>
2026-01-21 12:19:07 +0300

    update

--- src/App.js
+++ src/App.js
@@ -118,43 +118,41 @@ function App() {
           {/* AI & MAIN PROJECTS */}
          <Project img={process.env.PUBLIC_URL +"/img/sky.png" }
           title="Atmos" 
-          imghight="260"
-          padding="0"
-          paddingdis="110px"
-           github="https://github.com/sedraosman/Atmos"
+          github="https://github.com/sedraosman/Atmos"
           description= "A weather web application developed with React, providing real-time weather information with bilingual support."/>
-
-     <Project 
-      img={process.env.PUBLIC_URL +"/img/products.png"}
-          title="Gold Shop" 
-          imghight="140"
-          padding="75px"
-          github="https://github.com/sedraosman/goldShop"
-          description={"A React web application that displays products from JSON data and dynamically calculates and shows gold prices fetched from the web."}/>
 
 
       <Project 
+      img={process.env.PUBLIC_URL +"/img/pray.png"} 
+          title="Prayer Times" 
+          github="https://github.com/sedraosman/prayer-times"
+          description={"A React web application that retrieves daily prayer times from an external API, displays them clearly, and calculates the real-time countdown to the next upcoming prayer."}/>
+        
+
+     
+      <Project 
       img={process.env.PUBLIC_URL +"/img/smartDay.png"}
           title="Smart Daily Assistant" 
-          imghight="250px"
-          padding="0"
           github="https://github.com/sedraosman/Smart-Daily-Assistant"
           description={"A mobile AI daily assistant app built with React Native that lets users write daily notes, analyze emotions with AI, and track mood changes over time."}/>
 
       <Project 
+      img={process.env.PUBLIC_URL +"/img/products.png"}
+          title="Gold Shop" 
+          github="https://github.com/sedraosman/goldShop"
+          description={"A React web application that displays products from JSON data and dynamically calculates and shows gold prices fetched from the web."}/>
+
+
+      
+      <Project 
       img={process.env.PUBLIC_URL +"/img/basket.png"} 
           title="Basketball Shot Detector" 
-          imghight="220px"
-          padding="0"
           github="https://github.com/sedraosman/Basketball-Shot-Detector"
           description={"A Python computer vision project that analyzes basketball videos to detect shots using color tracking and trajectory modeling with a simple GUI."}/>
 
       <Project 
       img={process.env.PUBLIC_URL +"/img/e-commerce.png"} 
           title="e-commerce" 
-          imghight="145px"
-          padding="70px"
-          paddingdis="150px"
           github="https://github.com/sedraosman/e-commerce2"
           description={"A web application developed with Laravel featuring product display, shopping cart, checkout, and a basic admin dashboard."}/>
  
@@ -162,29 +160,16 @@ function App() {
        <Project 
       img={process.env.PUBLIC_URL +"/img/student.png"}
           title="Studend Meeting" 
-          imghight="170px"
-          padding="50px"
           github="https://github.com/sedraosman/studend-rendezvous"
           description={" A Java appointment booking system for students and professors, supporting selection of available time slots and management of meeting requests."}/>
 
       <Project 
       img={process.env.PUBLIC_URL +"/img/task.png"} 
           title="My Tasks" 
-          imghight="200px"
-          padding="20px"
-          paddingdis="145px"
           github="https://github.com/sedraosman/My_Tasks"
           description={"A task management web application developed with React, allowing users to add, edit, and delete daily tasks."}/>
 
-       <Project 
-      img={process.env.PUBLIC_URL +"/img/pray.png"} 
-          title="Prayer Times" 
-          imghight="200px"
-          padding="20px"
-          paddingdis="145px"
-          github="https://github.com/sedraosman/prayer-times"
-          description={"A React web application that retrieves daily prayer times from an external API, displays them clearly, and calculates the real-time countdown to the next upcoming prayer."}/>
-        </div>
+       </div>
       </section>
 
       {/* ================= FOOTER ================= */}
--- src/Project.js
+++ src/Project.js
@@ -1,4 +1,3 @@
-
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -6,39 +5,63 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardActions from '@mui/material/CardActions';
-export default function Project( {img,title,description,imghight,padding,github,paddingdis}) {
+
+export default function Project({ img, title, description, github }) {
   return (
-   <>
-   <Card sx={{ maxWidth: 280 ,margin:5 , maxHeight:490 }}>
-      <CardActionArea>
-      <CardMedia
-      component="img"
-      image={img}
-      alt="Sky"
+    <Card
       sx={{
-        height: imghight,
-        width: "100%",
-       objectFit: "cover",   // 🔥 beyaz kenar yok
+        width: { xs: '90%', sm: 350 },   
+        height: 500,
+        display: 'flex',
+        flexDirection: 'column',
+        m: 2,
       }}
-    />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div"sx={{  paddingTop: padding }}>
-           {title}
+    >
+      <CardActionArea sx={{ flexGrow: 1 }}>
+        {/* IMAGE */}
+        <CardMedia
+          component="img"
+          image={img}
+          alt={title}
+          sx={{
+            height: 260,
+            objectFit: 'contain',     
+            backgroundColor: '#fffff',    
+            p: 1,
+          }}
+        />
+
+        {/* CONTENT */}
+        <CardContent
+          sx={{
+            flexGrow: 1,
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <Typography gutterBottom variant="h6" align="center">
+            {title}
           </Typography>
-          <Typography variant="body1" sx={{ color: 'text.secondary',height:paddingdis}}>
-           {description}
+
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{
+              flexGrow: 1,
+              textAlign: 'center',
+            }}
+          >
+            {description}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary" href = {github} >
-          github 
+
+      {/* ACTIONS */}
+      <CardActions sx={{ justifyContent: 'center' }}>
+        <Button size="small" href={github} target="_blank">
+          GitHub
         </Button>
       </CardActions>
     </Card>
-   
-   
-   
-   </>
-  )
+  );
 }
